db57a3775e8cb002b4586dcbdb82fc8ccdc2ff8a
Joao <palotti@macondo.(none)>
2012-08-17 17:07:06 +0200

    First changes (after months) in the initial form.

--- js/process.js
+++ js/process.js
@@ -11,6 +11,27 @@ function login() {
 }
 
 function register() {
-  alert("Registration not implemented yet")
+  //  alert("Registration not implemented yet")
   // TODO: register this user (save information on DB)
+  
+  var name = $("#username").val().toString();
+  var email = $("#email").val().toString();
+//TODO: check email using regexps
+
+  var pass = $(".passToCheck:eq(0)").val().toString();
+  var passc = $(".passToCheck:eq(1)").val().toString();
+  
+  //var pass = $("#password:eq(0)").val().toString();
+  //var passc = $("#password:eq(1)").val().toString();
+ 
+  //TODO: check other variables...
+  if (pass != passc){
+    alert("The passwords do not match!")
+  }
+  
+  if (pass.length == 0){
+    alert("Your password should have at least one letter or number!")  
+  }
+
+
 }
